0d227326884ac15486132c3379a40943d315e68d
JoshAbar <20520746@gm.uit.edu.vn>
2022-10-19 12:17:30 +0700

    add API key

--- react-movie-main/src/api/apiConfig.js
+++ react-movie-main/src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
+    apiKey: 'cd570e2a0f781318cb353c1f9ce2f799',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
